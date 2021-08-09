e2e7d24187a1b116e45e41a36580d2a6c6e6fc05
Mr.喜 <863450594@qq.com>
2021-08-09 10:08:02 +0800

    完善菜单下拉框

--- src/layout/app/AppBar.vue
+++ src/layout/app/AppBar.vue
@@ -27,6 +27,10 @@
           <v-icon>mdi-account</v-icon>
           <v-list-item-title>个人信息</v-list-item-title>
         </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-account-lock</v-icon>
+          <v-list-item-title>修改密码</v-list-item-title>
+        </v-list-item>
         <v-divider />
         <v-list-item @click="logout">
           <v-icon>mdi-logout</v-icon>
@@ -34,13 +38,13 @@
         </v-list-item>
       </v-list>
     </v-menu>
-    <screen-full />
     <v-menu
       transition="slide-y-transition"
       close-on-content-click
       offset-y
       bottom
-      nudge-left="36"
+      nudge-left="136"
+      nudge-top="-10"
     >
       <template v-slot:activator="{ on }">
         <v-btn icon large dark v-on="on">
@@ -49,13 +53,23 @@
           </v-icon>
         </v-btn>
       </template>
-      <v-card>
-
+      <v-card width="300" style="padding: 15px; text-align:center;">
+        <v-row v-for="row in application" :key="row">
+          <v-col v-for="col in row" :key="col" style="margin:5px; border-radius: 10px;">
+            <a :href="col.href" style="display: block; width: 100%">
+              <v-icon>
+                {{ col.icon }}
+              </v-icon>
+              <div>{{ col.title }}</div>
+            </a>
+          </v-col>
+        </v-row>
       </v-card>
     </v-menu>
     <v-btn icon>
       <svg-icon name="bell" />
     </v-btn>
+    <screen-full />
   </v-app-bar>
 </template>
 
@@ -69,23 +83,36 @@ export default {
     SvgIcon,
     ScreenFull
   },
-  data: () => ({}),
+  data() {
+    return {
+      application: [
+        [{ href: '/#/download', icon: 'mdi-download', title: '下载' }, { href: '/#/github', icon: 'mdi-github', title: 'Github' }, { href: '/#/poolPerview', icon: 'mdi-eye-settings-outline', title: '卡池预览' }],
+        [{ href: '#', icon: 'mdi-apps', title: 'aaa' }, { href: '#', icon: 'mdi-apps', title: 'bbb' }, { href: '#', icon: 'mdi-apps', title: 'ccc' }],
+        [{ href: '#', icon: 'mdi-apps', title: 'aaa' }, { href: '#', icon: 'mdi-apps', title: 'bbb' }, { href: '#', icon: 'mdi-apps', title: 'ccc' }]
+      ]
+    }
+  },
   methods: {
     navIconClick() {
       this.$store.dispatch('app/toggleSideBar')
     },
     logout() {
-      this.$store.dispatch('user/logout', this.$store.getters.token).then(() => {
-        // 刷新路由
-        history.go('/login')
-      })
+      this.$store
+        .dispatch('user/logout', this.$store.getters.token)
+        .then(() => {
+          // 刷新路由
+          history.go('/login')
+        })
     }
   }
 }
 </script>
 
 <style scoped>
-.v-list-item{
-  cursor: pointer;
+a{
+  color: black !important;
+}
+.row> .col:hover{
+  background-color: rgb(232,240,254);
 }
 </style>
